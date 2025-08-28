5cc7e1c54fb7ead0077c3792ff7d793d6eea1b15
TimoSR <timothy.s.rasmussen@gmail.com>
2025-08-28 22:25:14 +0200

    init

---
+++ HackerRank/HackerRank/HackerRankEasy.cs
@@ -0,0 +1,21 @@
+﻿namespace HackerRank;
+
+public class HackerRankEasy
+{
+    public int solveMeFirst(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int simpleArraySum(List<int> ar)
+    {
+        int sum = 0;
+
+        foreach (var value in ar)
+        {
+            sum += value;
+        }
+        
+        return sum;
+    }
+}
---
+++ HackerRank/TestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using HackerRank;
+
+namespace TestProject1;
+
+public class UnitTest1
+{
+    
+    private readonly HackerRankEasy _hackerRank = new();
+
+    [Fact]
+    public void SolveMeFirst_WhenAddingPositiveNumbers_ReturnsSum() => Assert.Equal(4, _hackerRank.solveMeFirst(2, 2));
+
+    [Fact]
+    public void SimpleArraySum_ReturnsSum()
+    {
+        var values = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        var result = _hackerRank.simpleArraySum(values);
+        
+        Assert.Equal(45, result);
+    }
+}
